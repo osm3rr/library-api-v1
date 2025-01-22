1ec32a669c6862544815b2f79edef0d46874fea1
osm3rr <osm3rr@adakademy.com>
2025-01-22 17:46:32 -0400

    endpoint list-books

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
     'books',
+    'apis',
+    # 3rd party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
